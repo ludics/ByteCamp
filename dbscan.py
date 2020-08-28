1e74810fc0300bde86116dca187fb28677c58dbb
ludics <ludi7319@163.com>
2020-08-29 01:41:24 +0800

    modify dbscan

--- dbscan.py
+++ dbscan.py
@@ -1,13 +1,15 @@
 from sklearn.cluster import DBSCAN
 import scipy.sparse as sp
 import numpy as np
+import sys
 
 
 if __name__ == "__main__":
     X = sp.load_npz('/root/workspace/GraphCluster/month_dis.npz')
-
+    eps = float(sys.argv[1])
+    min_samples = int(sys.argv[2])
     # Compute DBSCAN
-    clustering = DBSCAN(eps=0.1, min_samples=10, metric='precomputed', n_jobs=10).fit(X)
+    clustering = DBSCAN(eps=eps, min_samples=min_samples, metric='precomputed', n_jobs=10).fit(X)
     labels = clustering.labels_
     
     # Number of clusters in labels, ignoring noise if present.
@@ -15,5 +17,5 @@ if __name__ == "__main__":
     n_noise_ = list(labels).count(-1)
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    np.save('./result/month_result_0.1_10.npy', labels)
-    np.save('./result/month_core_0.1_10.npy', clustering.core_sample_indices_)
+    np.save('./result/month_result_{}_{}.npy'.format(eps, min_samples), labels)
+    np.save('./result/month_core_{}_{}.npy'.format(eps, min_samples), clustering.core_sample_indices_)
