1f7cd412f99a0343e5a31fe983cd07c8a588d881
ludics <ludi7319@163.com>
2020-08-29 16:32:48 +0800

    modify sparse

--- make_sparse_affinity.py
+++ make_sparse_affinity.py
@@ -2,26 +2,47 @@ import pandas as pd
 import scipy.sparse as sp
 import numpy as np
 import time
+from multiprocessing import Pool
+
+start = time.time()
+year_data = pd.read_csv('/data00/tiger/graph_data/year_gid_uid_pair.csv', header=None)
+year_data.columns = ['gid1', 'uid1', 'gid2', 'uid2', 'weight']
+print("Read csv {}s".format(time.time() - start))
+
+
+def make_dict(num):
+    global year_data
+    dis_dict = {}
+    split_num = int(np.ceil(year_data.shape[0]) / 46. )
+    start = split_num * num
+    end = min( split_num * (num + 1), year_data.shape[0])
+    start = time.time()
+    for i in range(start, end):
+        dis_dict[(year_data['gid1'][i] - 1, year_data['gid2'][i] - 1)] = year_data['weight'][i]
+        dis_dict[(year_data['gid2'][i] - 1, year_data['gid1'][i] - 1)] = year_data['weight'][i]
+        if i % 1000000 == 0:
+            print("{} done {} in {}s".format(num, i, time.time() - start))
+    print("{} all done in {}s".format(num, time.time() - start))
+    return dis_dict
 
 
 if __name__ == "__main__":
-    start = time.time()
-    year_data = pd.read_csv('/data00/tiger/graph_data/year_gid_uid_pair.csv', header=None)
-    print("Read csv {}s".format(time.time() - start))
-    year_data.columns = ['gid1', 'uid1', 'gid2', 'uid2', 'weight']
     dis_dict = {}
     start = time.time()
-    for i in range(year_data.shape[0]):
-        dis_dict[(year_data['gid1'][i] - 1, year_data['gid2'][i] - 1)] = year_data['weight'][i]
-        dis_dict[(year_data['gid2'][i] - 1, year_data['gid1'][i] - 1)] = year_data['weight'][i]
-        if i % 10000000 == 0:
-            print("{} done {}s".format(i, time.time() - start))
+    result = []
+    pool = Pool(processes=46)
+    for i in range(46):
+        result.append(pool.apply_async(make_dict, (i, )))
+    pool.close()
+    pool.join()
+    for res in result:
+        dis_dict.update(res.get())
+    # del year_data
     start = time.time()
     for i in range(60034550):
         dis_dict[(i, i)] = 1.0
         if i % 10000000 == 0:
-            print("{} 2 done {}s".format(i, time.time() - start))
-    del year_data
+            print("Add diag done {} in {}s".format(i, time.time() - start))
     print("make dict end")
     start = time.time()
     values = list(dis_dict.values())
