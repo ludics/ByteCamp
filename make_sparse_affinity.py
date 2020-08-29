70fd99506a42666f144f387b3ff8e093882ec9a7
ludics <ludi7319@163.com>
2020-08-29 16:52:05 +0800

    modify sparse

--- make_sparse_affinity.py
+++ make_sparse_affinity.py
@@ -4,22 +4,16 @@ import numpy as np
 import time
 from multiprocessing import Pool
 
-start = time.time()
-year_data = pd.read_csv('/data00/tiger/graph_data/year_gid_uid_pair.csv', header=None)
-year_data.columns = ['gid1', 'uid1', 'gid2', 'uid2', 'weight']
-print("Read csv {}s".format(time.time() - start))
 
 
-def make_dict(num):
-    global year_data
+
+def make_dict(split_year_data):
     dis_dict = {}
-    split_num = int(np.ceil(year_data.shape[0]) / 46. )
-    start = split_num * num
-    end = min( split_num * (num + 1), year_data.shape[0])
     start = time.time()
-    for i in range(start, end):
-        dis_dict[(year_data['gid1'][i] - 1, year_data['gid2'][i] - 1)] = year_data['weight'][i]
-        dis_dict[(year_data['gid2'][i] - 1, year_data['gid1'][i] - 1)] = year_data['weight'][i]
+    num = split_year_data.index[0] / 11992875
+    for i in range(split_year_data.shape[0]):
+        dis_dict[(split_year_data['gid1'].iloc[i] - 1, split_year_data['gid2'].iloc[i] - 1)] = split_year_data['weight'].iloc[i]
+        dis_dict[(split_year_data['gid2'].iloc[i] - 1, split_year_data['gid1'].iloc[i] - 1)] = split_year_data['weight'].iloc[i]
         if i % 1000000 == 0:
             print("{} done {} in {}s".format(num, i, time.time() - start))
     print("{} all done in {}s".format(num, time.time() - start))
@@ -27,12 +21,20 @@ def make_dict(num):
 
 
 if __name__ == "__main__":
+    start = time.time()
+    year_data = pd.read_csv('/data00/tiger/graph_data/year_gid_uid_pair.csv', header=None)
+    year_data.columns = ['gid1', 'uid1', 'gid2', 'uid2', 'weight']
+    print("Read csv {}s".format(time.time() - start))
+
     dis_dict = {}
     start = time.time()
     result = []
     pool = Pool(processes=46)
     for i in range(46):
-        result.append(pool.apply_async(make_dict, (i, )))
+        split_num = int(np.ceil(year_data.shape[0]) / 46. )
+        begin = split_num * i
+        end = min( split_num * (i + 1), year_data.shape[0])
+        result.append(pool.apply_async(make_dict, (year_data[:][begin:end], )))
     pool.close()
     pool.join()
     for res in result:
