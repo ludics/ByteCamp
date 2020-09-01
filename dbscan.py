28888f90153aea80e9bf99d37d8346dcf0473a9b
芦迪 <ludi@ludideMacBook-Pro.local>
2020-09-01 15:02:51 +0800

    update dbscan mp

--- dbscan.py
+++ dbscan.py
@@ -7,28 +7,29 @@ import time
 
 
 paramenter_list = [
-    # (0.8, 1),
+    (0.8, 1),
     # (0.2, 1),
     # (0.1, 1),
     # (0.05, 1),
     # (0.01, 1),
+    (0.8, 2),
     # (0.2, 2),
     # (0.1, 2),
     # (0.05, 2),
     # (0.01, 2),
-    (0.8, 5),
+    # (0.8, 5),
     # (0.2, 5),
     # (0.1, 5),
     # (0.05, 5),
     # (0.01, 5),
-    # (0.8, 10),
+    (0.8, 10),
     # (0.2, 10),
-    (0.1, 10),
-    (0.05, 10)
+    # (0.1, 10),
+    # (0.05, 10),
     # (0.01, 10)
 ]
 
-X = sp.load_npz('/root/workspace/GraphCluster/month_dis.npz')
+X = sp.load_npz('/root/workspace/GraphCluster/year_dis.npz')
 
 
 def make_dbscan(num):
@@ -41,8 +42,8 @@ def make_dbscan(num):
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
     # print('{}Estimated number of noise points: %d'.format(n_noise_))
-    np.save('./result/month_result_{}_{}.npy'.format(eps, min_samples), labels)
-    np.save('./result/month_core_{}_{}.npy'.format(eps, min_samples), clustering.core_sample_indices_)   
+    np.save('./result/year_result_{}_{}.npy'.format(eps, min_samples), labels)
+    np.save('./result/year_core_{}_{}.npy'.format(eps, min_samples), clustering.core_sample_indices_)   
     print('eps={}, min_samples={}, Estimated number of clusters: {}'.format(eps, min_samples, n_clusters_))
     print('eps={}, min_samples={}, Estimated number of noise points: {}'.format(eps, min_samples, n_noise_))
     print('eps={}, min_samples={}, time cost: {:.3f}'.format(eps, min_samples, time.time() - start))
