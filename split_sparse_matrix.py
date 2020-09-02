ed22d49372e2942b9841b29132676719d426eac5
ludics <me@ludics.cn>
2020-09-02 14:06:14 +0800

    update split

--- split_sparse_matrix.py
+++ split_sparse_matrix.py
@@ -1,9 +1,15 @@
 import sys
 import numpy as np
 import scipy.sparse as sp
+import time
 
 
 if __name__ == "__main__":
+    verbose = False
+    if sys.argv > 2:
+        verbose = True
+    start = time.time()
+
     labels_ori = np.load(sys.argv[1])
     labels = np.sort(labels_ori)
     count = 0
@@ -19,6 +25,10 @@ if __name__ == "__main__":
     size2num = {}
     for i in size_set:
         size2num[i] = np.where(size_list==i)[0].shape[0]
+    
+    if verbose:
+        print("Start at {:.3f}".format(time.time() - start))
+    start = time.time()
 
     cluster2gid = {}
     for i in range(labels_ori.shape[0]):
@@ -34,7 +44,7 @@ if __name__ == "__main__":
 
     for cluster_id, cluster_size in cluster2size.items():
         for k in range(len(level_pos)-1):
-            if level_pos[k] < cluster_size <= level_pos[i+1]:
+            if level_pos[k] < cluster_size <= level_pos[k+1]:
                 bucket2cluster[k].append(cluster_id)
     bucket2gid = {}
     for k in range(len(level_pos)-1):
@@ -42,6 +52,10 @@ if __name__ == "__main__":
     for k, cluster_id_list in bucket2cluster.items():
         for cluster_id in cluster_id_list:
             bucket2gid[k] += cluster2gid[cluster_id]
+
+    if verbose:
+        print("Make bucket2gid, bucket2cluster, bucket2eid at {:.3f}".format(time.time() - start))
+    start = time.time()
 
     X = sp.load_npz('/root/workspace/GraphCluster/sparse_matrix/year_dis.npz')
     X_coo = sp.coo_matrix(X)
@@ -67,6 +81,11 @@ if __name__ == "__main__":
         for i, gid in enumerate(gid_list):
             bucket2gidmap[k]['ori2re'][gid] = i
             bucket2gidmap[k]['re2ori'][i] = gid
+
+    if verbose:
+        print("Load X, make gid2eid, bucket2eid, bucket2gidmap at {:.3f}".format(time.time() - start))
+    start = time.time()
+
     bucket2X = {}
     for k in range(len(level_pos)-1):
         ori_row, ori_col = X_coo.row[bucket2eid[k]], X_coo.col[bucket2eid[k]]
@@ -75,5 +94,15 @@ if __name__ == "__main__":
             re_row[i] = bucket2gidmap[k]['ori2re'][ori_row[i]]
             re_col[i] = bucket2gidmap[k]['ori2re'][ori_col[i]]
         bucket2X[k] = sp.csr_matrix((X_coo.data[bucket2eid[k]], (re_row, re_col)))    
+    
+    if verbose:
+        print("Make bucket2X at {:.3f}".format(time.time() - start))
+    start = time.time()
+
     for k, X in bucket2X.items():
         sp.save_npz('/root/workspace/GraphCluster/sparse_matrix/year_bucket{}_dis.npz'.format(k), X)
+
+    if verbose:
+        print("Save & all done at {:.3f}".format(time.time() - start))
+    start = time.time()
+
