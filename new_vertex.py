73dde39ad8c3460fb05ec26c19444a97d32c544d
Hao Guo <haoguo@HaodeMacBook-Pro.local>
2020-09-02 12:53:10 +0800

    Debug

--- new_vertex.py
+++ new_vertex.py
@@ -9,23 +9,24 @@ def change_g(gid, edge_list, g):
 
 #When a new vertex with gid and edge_list comes, change_g(gid, edge_list, g)
 
-def subgraph_to_solve(gid, edge_list, c, clusters, g):
+def subgraph_to_solve(gid, edge_list, labels, cluster2gids, g):
     '''
         gid is the new vertex's gid
         edge_list saves all the edges connect this new vertex with the origin graph
-        c[i] claims which cluster video i is in
-        clusters[i] saves the list of video gids in the i-th cluster
+        labels[i] claims which cluster video i is in
+        cluster2gids[i] saves the list of video gids in the i-th cluster
         g is the graph
 
         the second returned value is an alias sparse matrix saving the subgraph to solve
         the first returned v_list saves the correspondence of the alias
     '''
     v_list = [gid]
+    labels.append(-1)
     for u in edge_list:
-        if c[u]==-1: v_list.append(u)
-        else:
-            v_list = v_list + clusters[c[u]]
-            del clusters[c[u]]
+        if labels[u]==-1: v_list.append(u)
+        elif labels[u] in cluster2gids:
+            v_list = v_list + cluster2gids[labels[u]]
+            del cluster2gids[labels[u]]
     dict = {}
     for i, v in enumerate(v_list):
         dict[v] = i
@@ -35,26 +36,26 @@ def subgraph_to_solve(gid, edge_list, c, clusters, g):
             if u in v_list:
                 row.append(dict[v])
                 col.append(dict[u])
-                value.append(e_list[v][u])
+                value.append(g[v][u])
     
-    return v_list, sp.csr_matrix((value, (row, col)))
+    return v_list, sp.csr_matrix((value, (row, col))) if value else None
 
 
-def new_vertex(gid, edge_list, g, c, clusters):
+def new_vertex(gid, edge_list, g, labels, cluster2gids):
     ''' a new vertex with gid and edge_list comes '''
     change_g(gid, edge_list, g)
-    v_list, X = subgraph_to_solve(gid, edge_list, c, clusters, g)
-    
+    v_list, X = subgraph_to_solve(gid, edge_list, labels, cluster2gids, g)
+    if X:
 #DBSCAN the subgraph and maintain the global c and clusters.
-    size = len(v_list)
-    clustering = DBSCAN(eps=eps(size), min_samples=min_samples(size), metric='precompute    d', n_jobs=10).fit(X)
-    labels = clustering.labels_
-    m = max(c)+1
-#with v_list[i] we can find the origin gid for alias i in the whole graph.
-    for i in range(size):
-        c[v_list[i]] = labels[i]+m
-        if (labels[i]+m) in clusters:
-            clusters[labels[i]+m].append(v_list[i])
-        else:
-            clusters[labels[i]+m] = [v_list[i]]
+        size = len(v_list)
+        clustering = DBSCAN(eps=eps(size), min_samples=min_samples(size), metric='precomputed', n_jobs=10).fit(X)
+        local_labels = clustering.labels_
+        m = max(labels)+1
+        
+        for i in range(size):
+            labels[v_list[i]] = local_labels[i]+m
+            if (local_labels[i]+m) in cluster2gids:
+                cluster2gids[local_labels[i]+m].append(v_list[i])
+            else:
+                cluster2gids[local_labels[i]+m] = [v_list[i]]
 
