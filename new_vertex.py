9155ca8e18c99165aaed7181af718175e3dfaad0
Hao Guo <haoguo@HaodeMacBook-Pro.local>
2020-09-02 10:55:53 +0800

    Add a New Vertex

--- new_vertex.py
+++ new_vertex.py
@@ -6,11 +6,10 @@ def change_g(gid, edge_list, g):
     for u in edge_list:
         g[gid][u] = edge_list[u]
         g[u][gid] = edge_list[u]
-    return g
 
-#When a new vertex with gid and edge_list comes, let g = change_g(gid, edge_list, g)
+#When a new vertex with gid and edge_list comes, change_g(gid, edge_list, g)
 
-def subgraph_to_solve_after_new_vertex(gid, edge_list, c, clusters, g):
+def subgraph_to_solve(gid, edge_list, c, clusters, g):
     '''
         gid is the new vertex's gid
         edge_list saves all the edges connect this new vertex with the origin graph
@@ -24,7 +23,9 @@ def subgraph_to_solve_after_new_vertex(gid, edge_list, c, clusters, g):
     v_list = [gid]
     for u in edge_list:
         if c[u]==-1: v_list.append(u)
-        else: v_list = v_list + clusters[c[u]]
+        else:
+            v_list = v_list + clusters[c[u]]
+            del clusters[c[u]]
     dict = {}
     for i, v in enumerate(v_list):
         dict[v] = i
@@ -39,6 +40,21 @@ def subgraph_to_solve_after_new_vertex(gid, edge_list, c, clusters, g):
     return v_list, sp.csr_matrix((value, (row, col)))
 
 
-
+def new_vertex(gid, edge_list, g, c, clusters):
+    ''' a new vertex with gid and edge_list comes '''
+    change_g(gid, edge_list, g)
+    v_list, X = subgraph_to_solve(gid, edge_list, c, clusters, g)
+    
 #DBSCAN the subgraph and maintain the global c and clusters.
+    size = len(v_list)
+    clustering = DBSCAN(eps=eps(size), min_samples=min_samples(size), metric='precompute    d', n_jobs=10).fit(X)
+    labels = clustering.labels_
+    m = max(c)+1
 #with v_list[i] we can find the origin gid for alias i in the whole graph.
+    for i in range(size):
+        c[v_list[i]] = labels[i]+m
+        if (labels[i]+m) in clusters:
+            clusters[labels[i]+m].append(v_list[i])
+        else:
+            clusters[labels[i]+m] = [v_list[i]]
+
