a9c9ff2f9f0ebed4de4c88e9694afae613b6fbf2
ludics <me@ludics.cn>
2020-09-02 13:21:08 +0800

    make bucket

--- split_sparse_matrix.py
+++ split_sparse_matrix.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import scipy.sparse as sp
 
+
 if __name__ == "__main__":
     labels_ori = np.load(sys.argv[1])
     labels = np.sort(labels_ori)
@@ -28,16 +29,16 @@ if __name__ == "__main__":
 
     level_pos = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 30000000]
     bucket2cluster = {}
-    for i in range(len(level_pos)-1):
-        bucket2cluster[i] = []
+    for k in range(len(level_pos)-1):
+        bucket2cluster[k] = []
 
     for cluster_id, cluster_size in cluster2size.items():
-        for i in range(len(level_pos)-1):
-            if level_pos[i] < cluster_size <= level_pos[i+1]:
-                bucket2cluster[i].append(cluster_id)
+        for k in range(len(level_pos)-1):
+            if level_pos[k] < cluster_size <= level_pos[i+1]:
+                bucket2cluster[k].append(cluster_id)
     bucket2gid = {}
-    for i in range(len(level_pos)-1):
-        bucket2gid[i] = []
+    for k in range(len(level_pos)-1):
+        bucket2gid[k] = []
     for k, cluster_id_list in bucket2cluster.items():
         for cluster_id in cluster_id_list:
             bucket2gid[k] += cluster2gid[cluster_id]
@@ -52,13 +53,27 @@ if __name__ == "__main__":
             gid2eid[X_coo.row[i]] = [i]
 
     bucket2eid = {}
-    for i in range(len(level_pos)-1):
-        bucket2eid[i] = []
+    for k in range(len(level_pos)-1):
+        bucket2eid[k] = []
     for k, gid_list in bucket2gid.items():
         for gid in gid_list:
             bucket2eid[k] += gid2eid[gid]
+    bucket2gidmap = {}
+    for k in range(len(level_pos)-1):
+        bucket2gidmap[k] = {}
+        bucket2gidmap[k]['ori2re'] = {}
+        bucket2gidmap[k]['re2ori'] = {}
+    for k, gid_list in bucket2gid.items():
+        for i, gid in enumerate(gid_list):
+            bucket2gidmap[k]['ori2re'][gid] = i
+            bucket2gidmap[k]['re2ori'][i] = gid
     bucket2X = {}
-    for i in range(len(level_pos)-1):
-        bucket2X[i] = sp.csr_matrix((X_coo.data[bucket2eid[i]], (X_coo.row[bucket2eid[i]], X_coo.col[bucket2eid[i]])))    
+    for k in range(len(level_pos)-1):
+        ori_row, ori_col = X_coo.row[bucket2eid[k]], X_coo.col[bucket2eid[k]]
+        re_row, re_col = ori_row.copy(), ori_col.copy()
+        for i in range(ori_col.shape[0]):
+            re_row[i] = bucket2gidmap[k]['ori2re'][ori_row[i]]
+            re_col[i] = bucket2gidmap[k]['ori2re'][ori_col[i]]
+        bucket2X[k] = sp.csr_matrix((X_coo.data[bucket2eid[k]], (re_row, re_col)))    
     for k, X in bucket2X.items():
         sp.save_npz('../GraphCluster/sparse_matrix/year_bucket{}_dis.npz'.format(k), X)
