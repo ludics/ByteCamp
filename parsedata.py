da364a45f5c37a40ded16bd225c578f9057c0065
Junyu <junyu@JunyudeMacBook-Pro.local>
2020-09-02 15:54:53 +0800

    fix convert to 32

--- parsedata.py
+++ parsedata.py
@@ -3,16 +3,24 @@ import collections
 import sys
 import json
 import pickle
+import pandas 
+import tqdm
 
 if __name__ == '__main__':
 	datafile = sys.argv[1]
 	with open(datafile, 'r') as f:
-		datas = csv.reader(f)
+		# datas = csv.reader(f)
+		datas = pd.read_csv('/data00/tiger/graph_data/year_gid_uid_pair.csv', header=None,
+			dtype={0: "int32", 1: "int32", 2: "int32", 3: "int32", 4: "float32"})
+    	datas.columns = ['gid1', 'uid1', 'gid2', 'uid2', 'weight']
 		edge_nums = 0
 		v2e_map = collections.defaultdict(dict)
 		u2v_map = collections.defaultdict(set)
-		for gid1, uid1, gid2, uid2, w in datas:
-			gid1, uid1, gid2, uid2, w = int(gid1), int(uid1), int(gid2), int(uid2), float(w)
+		print("[DEBUG] DATA LOADED!")
+		for gid1, uid1, gid2, uid2, w in tqdm.tqdm(datas):
+			print(gid, uid1, gid2, uid2, w)
+			exit(0)
+			gid1, uid1, gid2, uid2, w = np.int(gid1), int(uid1), int(gid2), int(uid2), float(w)
 			# import ipdb;ipdb.set_trace()
 			if min(gid1, gid2) in v2e_map[max(gid1, gid2)]:
 				continue
@@ -42,6 +50,8 @@ if __name__ == '__main__':
 
 		# with open(datafile.replace('.csv', '_u2v.json'), 'w') as f:
 		# 	json.dump(u2v_map, f, indent=4)
+
+		print("[DEBUG] BEGIN SAVE !")
 		
 		with open(datafile.replace('.csv', '_v2e_32.pkl'), 'wb') as f:
 			pickle.dump(v2e_map, f, protocol = pickle.HIGHEST_PROTOCOL)
