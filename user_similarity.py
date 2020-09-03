18de011a4b0f6579e1e8f9a528d30517e2e5823d
ludics <me@ludics.cn>
2020-09-03 19:33:32 +0800

    update user_simi

--- user_similarity.py
+++ user_similarity.py
@@ -1,27 +1,37 @@
 import tqdm
+import gc
+import scipy.sparse as sp
+from user_cluster import *
+import numpy as np
+
 
 def make_user_similarity_graph(cluster_by_user):
     '''
         there are cluster_by_user[i][j] works in cluster[i] made by user[j]
     '''
-    u_graph = {}
-    for i in tqdm.tqdm(cluster_by_user):
-        amp = sum(v for v in cluster_by_user[i].values())
-        s = sum(v*v for v in cluster_by_user[i].values())
-        tmp = {}
-        for j in cluster_by_user[i]:
-            #if cluster_by_user[i][j]*cluster_by_user[i][j]*amp/s>0.01:
-            tmp[j] = cluster_by_user[i][j]/s
-        for i in tmp:
-            for j in tmp:
+    rows, cols, vals = [], [], []
+    for cluster_id in tqdm.tqdm(cluster_by_user):
+        amp = sum(v for v in cluster_by_user[cluster_id].values())
+        for i in cluster_by_user[cluster_id]:
+            for j in cluster_by_user[cluster_id]:
                 if i!=j:
-                    if i not in u_graph:
-                        u_graph[i] = {j:tmp[i]*tmp[j]*amp}
-                    elif j in u_graph[i]:
-                        u_graph[i][j] += tmp[i]*tmp[j]*amp
-                    else:
-                        u_graph[i][j] = tmp[i]*tmp[j]*amp
-    return u_graph
+                    rows.append(i)
+                    cols.append(j)
+                    vals.append(cluster_by_user[cluster_id][i]*cluster_by_user[cluster_id][j]/amp)
+        if (cluster_id + 1) % 2000000 == 0:
+            X = sp.csr_matrix((vals, (rows, cols)))
+            sp.save_npz('/root/workspace/GraphCluster/user_feature/user_similiar/{}.npy'.format(cluster_id), X)
+            del X
+            del rows
+            del cols
+            del vals
+            gc.collect()
+            rows, cols, vals = [], [], []
+            print("Save & gc, cluster id: {}".format(cluster_id))
+    X = sp.csr_matrix((vals, (rows, cols)))
+    sp.save_npz('/root/workspace/GraphCluster/user_feature/user_similiar/{}.npy'.format(cluster_id), X)
+    print("Save & gc, cluster id: {}".format(cluster_id))
+    # return u_graph
 
 import math
 def compute_user_significance(v2u, cluster_to_gid):
@@ -35,3 +45,12 @@ def compute_user_significance(v2u, cluster_to_gid):
         for j in range(l):
             significance[v2u[tmp[j]]] += math.tan(-math.pi/4+math.pi/2*j/l)*l
     return significance
+
+
+if __name__ == "__main__":
+    v2u = np.load('/root/workspace/GraphCluster/adj_list/year_gid_uid_pair_v2u.npy')
+    labels = np.load('/root/workspace/GraphCluster/dbscan_result/10-bucket/0b_0.8_1_1b_0.8_1_2b_0.5_3_3b_0.5_3_4b_0.5_3_5b_0.6_3_6b_0.5_3_7b_0.5_3_8b_0.05_3_9b_0.005_1.npy')
+    user_in_cluster, cluster_by_user =  user_cluster_nums(labels, v2u)
+    del user_in_cluster
+    make_user_similarity_graph(cluster_by_user)
+
