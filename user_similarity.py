ba10a18d3acfa1e08d2985a3245d8d1d65dbcaac
root <root@debug010000002015.local.alipay.net>
2020-09-03 19:08:09 +0800

    use guohao's user_* files

--- user_similarity.py
+++ user_similarity.py
@@ -1,15 +1,17 @@
+import tqdm
+
 def make_user_similarity_graph(cluster_by_user):
     '''
         there are cluster_by_user[i][j] works in cluster[i] made by user[j]
     '''
     u_graph = {}
-    for i in cluster_by_user:
+    for i in tqdm.tqdm(cluster_by_user):
         amp = sum(v for v in cluster_by_user[i].values())
         s = sum(v*v for v in cluster_by_user[i].values())
         tmp = {}
         for j in cluster_by_user[i]:
-            if cluster_by_user[i][j]*cluster_by_user[i][j]*amp/s>0.01:
-                tmp[j] = cluster_by_user[i][j]/s
+            #if cluster_by_user[i][j]*cluster_by_user[i][j]*amp/s>0.01:
+            tmp[j] = cluster_by_user[i][j]/s
         for i in tmp:
             for j in tmp:
                 if i!=j:
