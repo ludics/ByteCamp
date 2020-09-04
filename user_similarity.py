37be079b34b09e5ae98880f65e4ac1f0dd7c883c
root <root@debug010000002015.local.alipay.net>
2020-09-04 09:08:39 +0800

    Debuged

--- user_similarity.py
+++ user_similarity.py
@@ -1,30 +1,29 @@
+import tqdm
+
 def make_user_similarity_graph(cluster_by_user):
     '''
         there are cluster_by_user[i][j] works in cluster[i] made by user[j]
     '''
     u_graph = {}
-    for i in cluster_by_user:
+    for cluster_id in tqdm.tqdm(cluster_by_user):
         amp = sum(v for v in cluster_by_user[i].values())
         s = sum(v*v for v in cluster_by_user[i].values())
-        tmp = {}
-        for j in cluster_by_user[i]:
-            if cluster_by_user[i][j]*cluster_by_user[i][j]*amp/s>0.01:
-                tmp[j] = cluster_by_user[i][j]/s
-        for i in tmp:
-            for j in tmp:
+        for i in cluster_by_user[cluster_id]:
+            for j in cluster_by_user[cluster_id]:
                 if i!=j:
                     if i not in u_graph:
-                        u_graph[i] = {j:tmp[i]*tmp[j]*amp}
+                        u_graph[i] = {j:tmp[i]*tmp[j]/amp}
                     elif j in u_graph[i]:
-                        u_graph[i][j] += tmp[i]*tmp[j]*amp
+                        u_graph[i][j] += tmp[i]*tmp[j]/amp
                     else:
-                        u_graph[i][j] = tmp[i]*tmp[j]*amp
+                        u_graph[i][j] = tmp[i]*tmp[j]/amp
     return u_graph
 
 import math
 def compute_user_significance(v2u, cluster_to_gid):
     '''
-        v2u[gid] = uid, cluster_to_gid[i] contains all the gids in cluster[i]
+        v2u[gid-1] = uid, cluster_to_gid[i] contains all the gids in cluster[i]
+        this function returns the relative significance of each user, computed by the rank of each work
     '''
     significance = [0.0]*4532375
     for i in cluster_to_gid:
